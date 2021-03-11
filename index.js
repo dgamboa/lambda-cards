8b90e0dea780fc223852a309266323aad05d008f
Daniel Gamboa <danielgamboa10@gmail.com>
2021-03-11 16:36:04 -0600

    Add heroku script to package.json and add generic endpoint to display react app in index.js

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 // Imports
 require('dotenv').config();
+const path = require('path');
 const express = require('express');
 
 // Express Server Instance and Middleware
 const server = express();
 
 server.use(express.json())
+server.use(express.static(path.join(__dirname, 'frontend/build')));
 
 if (process.env.NODE_ENV === 'development') {
   const cors = require('cors');
   server.use(cors());
 };
 
-// Endpoints
+// API Endpoints
+// ...to be written...
+
+// Display React App for non-API Endpoints
 server.use('*', (req, res) => {
-  res.send('<h1>Hello</h1>')
+  res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
 });
 
 // PORT Set Up and Listen
