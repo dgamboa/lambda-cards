bcd86c9f326b23ae503c327bda5884c761a0cf32
Daniel Gamboa <danielgamboa10@gmail.com>
2021-03-11 16:09:45 -0600

    Add env variables and set up port in index.js

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+require('dotenv').config();
 const express = require('express');
 
 const server = express();
 
 server.use(express.json())
 
+if (process.env.NODE_ENV === 'development') {
+  const cors = require('cors');
+  server.use(cors());
+};
+
+const PORT = process.env.PORT || 4000;
+
+server.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
