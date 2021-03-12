27eb543aebf8e6a3cf74120a262e7ef1befc6ef7
Daniel Gamboa <danielgamboa10@gmail.com>
2021-03-12 15:25:02 -0600

    Add conclusion page to App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { Route } from "react-router";
-import FlashCard from "./components/FlashCard";
 import HomePage from "./components/HomePage";
+import FlashCard from "./components/FlashCard";
+import ConclusionPage from "./components/ConclusionPage";
 
 function App() {
   return (
     <div className="App">
       <Route exact path={"/"} component={HomePage}/>
       <Route exact path={"/cards"} component={FlashCard}/>
+      <Route exact path={"/conclusion"} component={ConclusionPage}/>
     </div>
   );
 }
