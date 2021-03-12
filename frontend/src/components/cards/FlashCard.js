209c3e62f70fb222b12a1494fd64eda89bddda06
Daniel Gamboa <danielgamboa10@gmail.com>
2021-03-12 16:31:22 -0600

    WIP FlashCard -> the flipping effect works

--- frontend/src/components/cards/FlashCard.js
+++ frontend/src/components/cards/FlashCard.js
@@ -1,13 +1,39 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-export default function FlashCard() {
+function FlashCard(props) {
+  const { displayQuestion } = props;
+
+  const flipCard = e => {
+    e.target
+     .parentNode
+     .parentNode
+     .classList.toggle('flipped');
+  };
+
   return (
     <section className="flashcard-container">
       <div className="ring-fence">
         <div className="card">
-          <div></div>
+          <div className="card-face front">
+            <h3 className="question-front">{displayQuestion.question}</h3>
+            <p onClick={flipCard}>See answer</p>
+          </div>
+          <div className="card-face back">
+            <h3 className="question-back">{displayQuestion.question}</h3>
+            <p className="answer-back">{displayQuestion.answer}</p>
+          </div>
         </div>
       </div>
+      <button className="next-question">Next Question</button>
     </section>
   )
 }
+
+const mapStateToProps = state => {
+  return {
+    displayQuestion: state.displayQuestion
+  };
+};
+
+export default connect(mapStateToProps, {})(FlashCard);
