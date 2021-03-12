f92e6588532939480f5e29c04870fabd35150c65
Daniel Gamboa <danielgamboa10@gmail.com>
2021-03-12 15:55:17 -0600

    Change FlashCard name to FlashCardPage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Route } from "react-router";
 import HomePage from "./components/HomePage";
-import FlashCard from "./components/FlashCard";
+import FlashCardPage from "./components/cards/FlashCardPage";
 import ConclusionPage from "./components/ConclusionPage";
 
 function App() {
   return (
     <div className="App">
       <Route exact path={"/"} component={HomePage}/>
-      <Route exact path={"/cards"} component={FlashCard}/>
+      <Route exact path={"/cards"} component={FlashCardPage}/>
       <Route exact path={"/conclusion"} component={ConclusionPage}/>
     </div>
   );
