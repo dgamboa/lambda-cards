52231eac8b498c2b43834ebfc88ddc362eefe294
Daniel Gamboa <danielgamboa10@gmail.com>
2021-03-14 13:26:50 -0500

    Add wave hand effect to home page

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { useHistory } from 'react-router';
 import { getQuestions } from '../actions';
+import './HomePage.css';
 
 function HomePage(props) {
   const history = useHistory();
@@ -14,11 +15,11 @@ function HomePage(props) {
 
   return (
     <div className="home-container">
-      <h1>Welcome to Lambda Cards 👋</h1>
+      <h1><span className="wave">👋</span> Welcome to Lambda Cards</h1>
       <h2>
         An easy way to prepare for the Lambda Web Core Interview using <a href="https://en.wikipedia.org/wiki/Active_recall" target=
         "_blank" rel="noreferrer">Active Recall</a> and <a href="https://en.wikipedia.org/wiki/Spaced_repetition" target=
-        "_blank" rel="noreferrer">Spaced Repetition</a>
+        "_blank" rel="noreferrer">Spaced Repetition</a> to <span className="typed"></span>
       </h2>
       <button className="start-button" onClick={handleStart}>Click here to start</button>
     </div>
