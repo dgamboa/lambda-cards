9a7f1ad3151f05167ddfc2872bcb4d000e1d2deb
Daniel Gamboa <danielgamboa10@gmail.com>
2021-03-14 14:20:59 -0500

    Add span and make cursor blink with border CSS

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { useHistory } from 'react-router';
 import { getQuestions } from '../actions';
@@ -6,6 +6,22 @@ import './HomePage.css';
 
 function HomePage(props) {
   const history = useHistory();
+
+  const typeEffect = (span, round = 1) => {
+    console.log(span);
+    // if (round < 4) {
+    //   round++;
+    //   typeEffect(span, round)
+    // } else {
+    //   round = 1;
+    //   typeEffect(span, round)
+    // }
+  };
+
+  useEffect(() => {
+    const typed = document.querySelector('.typed');
+    typeEffect(typed);
+  }, [])
 
   const handleStart = e => {
     e.preventDefault();
@@ -19,7 +35,8 @@ function HomePage(props) {
       <h2>
         An easy way to prepare for the Lambda Web Core Interview using <a href="https://en.wikipedia.org/wiki/Active_recall" target=
         "_blank" rel="noreferrer">Active Recall</a> and <a href="https://en.wikipedia.org/wiki/Spaced_repetition" target=
-        "_blank" rel="noreferrer">Spaced Repetition</a> to <span className="typed"></span>
+        "_blank" rel="noreferrer">Spaced Repetition</a> to <span className="typed">💎🙌🚀🌝 it</span>
+        {/* pass it, rock it, 💎🙌🚀🌝 it */}
       </h2>
       <button className="start-button" onClick={handleStart}>Click here to start</button>
     </div>
