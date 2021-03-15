f1083204e67199a381a8060b381473a6335b00ce
Daniel Gamboa <danielgamboa10@gmail.com>
2021-03-14 19:41:22 -0500

    Remove windups

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import { useHistory } from 'react-router';
 import { getQuestions } from '../actions';
@@ -7,11 +7,17 @@ import './HomePage.css';
 function HomePage(props) {
   const history = useHistory();
 
+
   const handleStart = e => {
     e.preventDefault();
     props.getQuestions();
     history.push("/cards");
   };
+
+  const RewindButton = () => {
+    const rewind = useRewind();
+    return <button onClick={rewind}>{"Rewind"}</button>;
+  }
 
   return (
     <div className="home-container">
@@ -19,8 +25,8 @@ function HomePage(props) {
       <h2 className="tagline">
         An easy way to prepare for the Lambda Web Core Interview using <a href="https://en.wikipedia.org/wiki/Active_recall" target=
         "_blank" rel="noreferrer">Active Recall</a> and <a href="https://en.wikipedia.org/wiki/Spaced_repetition" target=
-        "_blank" rel="noreferrer">Spaced Repetition</a> to <span className="typed"></span>
-        {/* pass it, rock it, 💎🙌🚀🌝 it */}
+        "_blank" rel="noreferrer">Spaced Repetition</a> to <span className="typed">{windup}</span>
+        {/* pass it, rock it, crush it, 💎🙌🚀🌝 it */}
       </h2>
       <button className="start-button" onClick={handleStart}>Click here to start</button>
     </div>
