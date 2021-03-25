66966b43c87ed5586e77220471d38a7e2491ef94
Daniel Gamboa <danielgamboa10@gmail.com>
2021-03-25 08:55:06 -0500

    Update GA from all routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,19 +4,25 @@ import HomePage from "./components/HomePage";
 import FlashCardPage from "./components/cards/FlashCardPage";
 import ConclusionPage from "./components/ConclusionPage";
 import Footer from "./components/Footer";
-import ReactGA from 'react-ga';
+import ReactGA from "react-ga";
+import { createBrowserHistory } from "history";
+
+ReactGA.initialize("UA-192318099-1");
+const browserHistory = createBrowserHistory();
+browserHistory.listen((location, action) => {
+  ReactGA.pageview(location.pathname + location.search)
+})
 
 function App() {
   useEffect(() => {
-    ReactGA.initialize('UA-192318099-1');
-    ReactGA.pageview('/');
+    ReactGA.pageview(window.location.pathname + window.location.search);
   }, []);
 
   return (
     <div className="App">
-      <Route exact path={"/"} component={HomePage}/>
-      <Route exact path={"/cards"} component={FlashCardPage}/>
-      <Route exact path={"/conclusion"} component={ConclusionPage}/>
+      <Route exact path={"/"} component={HomePage} />
+      <Route exact path={"/cards"} component={FlashCardPage} />
+      <Route exact path={"/conclusion"} component={ConclusionPage} />
       <Footer />
     </div>
   );
