4a0797d0ce6a6e7903e1d492d2405756c545b524
Daniel Gamboa <danielgamboa10@gmail.com>
2021-05-22 10:23:21 -0500

    Edit message on DonatePage

--- frontend/src/components/DonatePage.js
+++ frontend/src/components/DonatePage.js
@@ -7,7 +7,7 @@ function DonatePage(props) {
       <h2 className="diamond-hands">💎 🙌 </h2>
       <h1>One-Time Donation</h1>
       <p className="tagline">
-        Donations like yours help maintain the site running so future <a href="https://lambdaschool.com/">Lambda School</a> students can use it to prepare for interviews
+        Donations like yours help maintain the site so future <a href="https://lambdaschool.com/">Lambda School</a> students can use it to prepare for interviews
       </p>
       <button className="donate-button" onClick={console.log("donate")}>Donate $1</button>
       <button className="donate-button" onClick={console.log("donate")}>Donate $5</button>
